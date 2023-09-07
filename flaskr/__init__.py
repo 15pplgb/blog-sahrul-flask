d04c97d2c332cbc44f98673d030f0c8fdce22d46
15pplgb <smknm9562@gmail.com>
2023-09-07 11:41:48 +0700

    add database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,14 @@
 # import alikasi flask untuk dipakai diweb kita
+# import aplikasi flask, os, flash, jsofiny, redirect, dan render_template untuk dipakai di web kita
+import os
+
+# import SQL untuk akses datbaase
+from cs50 import SQL
+# import flash untuk nontifikasi pada web
+# import jsofiny untuk memformat data
+# import redirect dan render_template untuk berpindah halaman web
+# import request dan session untuk mengakses data
+
 from flask import Flask
 
 # mengatur nama aplikasi
